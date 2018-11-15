263107d5aa082b346837d6af4dfe3630593fb341
luclecq <45034713+luclecq@users.noreply.github.com>
2018-11-15 13:49:05 +0100

    Add files via upload

---
+++ uitvoeren_script.bat
@@ -0,0 +1,42 @@
+rem =========================================================
+rem
+rem file: uitvoeren_script.txt
+rem van: Luc en Gerard
+rem d.d.: 15-11-18
+rem
+rem doel: start python script op installatie van Overijssel
+rem
+rem =========================================================
+
+@echo off
+
+cls
+
+echo.
+
+echo Automatische controle van metadatakwaliteit V0.4
+
+echo.
+echo.
+
+echo ========================================================
+ 
+echo.
+echo.
+
+pause
+
+echo.
+echo.
+
+C:\python27\arcgis10.4\python.exe <bestandspad pythonscript> <bestandspad input> <bestandspad output>
+
+echo.
+echo.
+
+echo ========================================================
+
+echo.
+echo.
+
+pause
